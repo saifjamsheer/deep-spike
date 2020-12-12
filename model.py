5946d3d3ba863ffbc68e0acfe578d8d9c173d4f2
Saif <saifjamsheer1998@gmail.com>
2020-12-12 17:43:55 +0300

    Started preprocessing

--- model.py
+++ model.py
@@ -1,38 +1,38 @@
 import numpy as np
 
-def initialize_parameters(n_x, n_h, n_y):
-    """
-    Argument:
-    n_x -- size of the input layer
-    n_h -- size of the hidden layer
-    n_y -- size of the output layer
+# def initialize_parameters(n_x, n_h, n_y):
+#     """
+#     Argument:
+#     n_x -- size of the input layer
+#     n_h -- size of the hidden layer
+#     n_y -- size of the output layer
     
-    Returns:
-    parameters -- python dictionary containing your parameters:
-                    W1 -- weight matrix of shape (n_h, n_x)
-                    b1 -- bias vector of shape (n_h, 1)
-                    W2 -- weight matrix of shape (n_y, n_h)
-                    b2 -- bias vector of shape (n_y, 1)
-    """
-    np.random.seed(1)
-    W1 = np.random.randn(n_h, n_x) * 0.01
-    b1 = np.zeros(shape=(n_h, 1))
-    W2 = np.random.randn(n_y, n_h) * 0.01
-    b2 = np.zeros(shape=(n_y, 1))
+#     Returns:
+#     parameters -- python dictionary containing your parameters:
+#                     W1 -- weight matrix of shape (n_h, n_x)
+#                     b1 -- bias vector of shape (n_h, 1)
+#                     W2 -- weight matrix of shape (n_y, n_h)
+#                     b2 -- bias vector of shape (n_y, 1)
+#     """
+#     np.random.seed(1)
+#     W1 = np.random.randn(n_h, n_x) * 0.01
+#     b1 = np.zeros(shape=(n_h, 1))
+#     W2 = np.random.randn(n_y, n_h) * 0.01
+#     b2 = np.zeros(shape=(n_y, 1))
 
-    assert(W1.shape == (n_h, n_x))
-    assert(b1.shape == (n_h, 1))
-    assert(W2.shape == (n_y, n_h))
-    assert(b2.shape == (n_y, 1))
+#     assert(W1.shape == (n_h, n_x))
+#     assert(b1.shape == (n_h, 1))
+#     assert(W2.shape == (n_y, n_h))
+#     assert(b2.shape == (n_y, 1))
     
-    parameters = {"W1": W1,
-                  "b1": b1,
-                  "W2": W2,
-                  "b2": b2}
+#     parameters = {"W1": W1,
+#                   "b1": b1,
+#                   "W2": W2,
+#                   "b2": b2}
     
-    return parameters
+#     return parameters
 
-def initialize_parameters_deep(layer_dims):
+def initialize_parameters(layer_dims):
     """
     Arguments:
     layer_dims -- python array (list) containing the dimensions of each layer in our network
@@ -55,5 +55,12 @@ def initialize_parameters_deep(layer_dims):
 
     return parameters
 
-parameters = initialize_parameters(2, 2, 1)
-parameters = initialize_parameters_deep([2,2,1])
+parameters = initialize_parameters([2,2,1])
+
+def forward(X, parameters):
+
+    return 1
+
+def cost(AL, y):
+
+    return 1
--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,6 @@
 import scipy.io as spio
+import matplotlib.pyplot as plt
+import numpy as np
 
 train_path = '/Users/saif/Desktop/University/Year 4/CI/Final/datasets/training.mat'
 submission_path = '/Users/saif/Desktop/University/Year 4/CI/Final/datasets/submission.mat'
@@ -8,3 +10,23 @@ d = mat['d'] # Raw time domain recording (1440000 samples), 25 kHz sampling freq
 Index = mat['Index'] # The location in the recording (in samples) of each spike.
 Class = mat['Class'] # The class (1, 2, 3 or 4), i.e the type of neuron that generated each spike.
 
+sf = 25000 # sampling frequency
+sec = len(d)/sf # duration of recording in seconds
+time = np.linspace(0, sec, len(d))
+
+fig, ax = plt.subplots(figsize=(15, 5))
+ax.plot(time[0:sf], d[0:sf])
+ax.set_title('Sampling Frequency: {}Hz'.format(sf))
+ax.set_xlim(0, time[sf])
+ax.set_xlabel('time [s]')
+ax.set_ylabel('amplitude [mV]')
+plt.show()
+
+def extract(data, window, tf, offset, ):
+    return 1
+
+def feature():
+    return 1
+
+def cluster():
+    return 1
